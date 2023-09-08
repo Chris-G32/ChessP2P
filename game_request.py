7eb734fb3a35e1825b54c8a7061a4671a5b212ff
Chris-G32 <cgruska@kent.edu>
2023-09-08 12:57:27 -0400

    added load from dict

--- game_request.py
+++ game_request.py
@@ -1,8 +1,12 @@
 from datetime import datetime
 
 class game_request:
-    def __init__(self,ip,user):
+    def __init__(self,ip:str,user:str,timestamp=datetime.now()):
         self.ip=ip
         self.user=user
-        self.timestamp=datetime.now()
-    
+        self.timestamp=timestamp
+    #this will break if attr names change
+    def load_from_dict(self,ob):
+        self.ip=ob['ip']
+        self.user=ob['user']
+        self.timestamp=ob['timestamp']
